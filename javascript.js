39ca0bd6334dd90cf058427877b751e41823850a
devloop <devloop307@gmail.com>
2026-02-05 01:44:07 -0500

    feat: agregar funcionalidad usuario ingresa su valor

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
 //Elegir valor maquina pidra, papel o tijera
 function getComputerChoice(){
     const opciones = ["piedra","papel","tijera"];
-    let valor = opciones[Math.floor(Math.random() * opciones.length)]
-    return console.log(valor)
+    let valor = opciones[Math.floor(Math.random() * opciones.length)];
+    return console.log(valor);
 }
 getComputerChoice()
 
 //Elegir usurio piedra, papel o tijera
+function getHumanChoice(){
+    let opcion = prompt('Ingrese su opción: "Piedra, papel o Tijera"'," ");
+    return console.log(opcion.toLowerCase())
+}
+
+getHumanChoice()
